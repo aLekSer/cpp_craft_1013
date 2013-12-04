4041eca8406540f48a139e534fae27d80101af17
Tramort <jake@gogo.by>
2013-12-05 01:26:36 +0300

    solutions/max_kaskevich/7/sources/minute_market_data/minute_market_data: added forwading messages to minute_calculator and writing datafeeds to files.

--- solutions/max_kaskevich/7/sources/minute_market_data/minute_market_data.cpp
+++ solutions/max_kaskevich/7/sources/minute_market_data/minute_market_data.cpp
@@ -2,25 +2,30 @@
 #include <string>
 
 
-minute_market::minute_market_data::minute_market_data( const std::string& dir_path,
-                                                       minute_market_calculator& calculator ) :
+minute_market::minute_market_data::minute_market_data( const std::string& dir_path ) :
     dir_path_( dir_path ),
-    calculator_( calculator )
+    calculator_( on_minute_data_feed )
 {
         
 }
 
 void minute_market::minute_market_data::new_trade( const trade_message_ptr& msg )
 {
-
+    calculator_.new_trade( msg );
 }
 
 void minute_market::minute_market_data::new_quote( const quote_message_ptr& msg )
 {
-
+    calculator_.new_quote( msg );
 }
 
 void minute_market::minute_market_data::on_minute_data_feed( const minute_datafeed& data )
 {
-
+    boost::mutex::scoped_lock lock( output_mtx_ );
+    std::ofstream& output = outputs_[ data.stock_name ];
+    if( !output.is_open() )
+    {
+        std::string path = dir_path_ + data.stock_name;
+        output.open( path );
+    }
 }
--- solutions/max_kaskevich/7/sources/minute_market_data/minute_market_data.h
+++ solutions/max_kaskevich/7/sources/minute_market_data/minute_market_data.h
@@ -4,6 +4,9 @@
 
 #include "market_data_processor.h"
 #include "minute_market_calculator.h"
+#include <map>
+#include <boost/thread.hpp>
+#include <fstream>
 
 using namespace multicast_communication;
 
@@ -16,9 +19,11 @@ namespace minute_market
 
         const std::string dir_path_;
         minute_market_calculator calculator_;
+
+        std::map< const std::string, std::ofstream > outputs_;
+        boost::mutex output_mtx_;
     public:
-        explicit minute_market_data( const std::string& dir_path,
-                                     minute_market_calculator& calculator );
+        explicit minute_market_data( const std::string& dir_path );
     private:
         // market_data_processor implementation
         void new_trade( const trade_message_ptr& msg );
