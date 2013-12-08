16a197f6cd4054eaac3176b41f52a3d39d7ffc88
Tramort <jake@gogo.by>
2013-12-09 00:16:46 +0300

    solutions/max_kaskevich/7/tests/minute_market_calculator_tests/minute_market_calculator_tests.cpp: improved tests.

--- solutions/max_kaskevich/7/tests/minute_market_calculator_tests/minute_market_calculator_tests.cpp
+++ solutions/max_kaskevich/7/tests/minute_market_calculator_tests/minute_market_calculator_tests.cpp
@@ -11,18 +11,18 @@ using namespace multicast_communication;
          BOOST_CHECK_NO_THROW( minute_market_calculator calc([]( const minute_datafeed& mdf ){}); )
      }
 
-    {
+     {
         bool callback_checked = false;
         minute_market_calculator calc( [&]( const minute_datafeed& mdf )
         {
-            BOOST_CHECK_EQUAL( mdf.close_prise, 77.9 );
+            BOOST_CHECK_EQUAL( mdf.close_prise, 88.9 );
             BOOST_CHECK_EQUAL( mdf.open_prise, 77.9 );
-            BOOST_CHECK_EQUAL( mdf.high_prise, 77.9 );
+            BOOST_CHECK_EQUAL( mdf.high_prise, 88.9 );
             BOOST_CHECK_EQUAL( mdf.low_price, 77.9 );
             BOOST_CHECK_EQUAL( std::string( mdf.stock_name ), std::string( "ACN" ) );
             BOOST_CHECK_EQUAL( mdf.minute, 36225u );
-            BOOST_CHECK_EQUAL( mdf.bid, 41.47 );
-            BOOST_CHECK_EQUAL( mdf.ask, 41.48 );
+            BOOST_CHECK_EQUAL( mdf.bid, 82.94 );
+            BOOST_CHECK_EQUAL( mdf.ask, 82.96 );
             std::ostringstream output;
             BOOST_CHECK_NO_THROW( output << mdf; )
             BOOST_CHECK_EQUAL( output.str().length(), 76 );
@@ -36,8 +36,13 @@ using namespace multicast_communication;
         }
         trade_message_ptr tm2( new trade_message() );
         {
-            std::istringstream input( "EIAO A  000146235T:5]008ACN@0100B00007790DD " );
+            std::istringstream input( "EIAO A  000146235T:3b008ACN@0100B00008890DD " );
             input >> *tm2;
+        }
+        trade_message_ptr tm3( new trade_message() );
+        {
+            std::istringstream input( "EIAO A  000146235T:5]008ACN@0100B00008890DD " );
+            input >> *tm3;
         }
 
         quote_message_ptr qm( new quote_message() );
@@ -45,14 +50,19 @@ using namespace multicast_communication;
             std::istringstream input( "EDEO A  003759032T:3]073ACNR  B00004147006 B00004148004 12" );
             input >> *qm;
         }
+        quote_message_ptr qm2( new quote_message() );
+        {
+            std::istringstream input( "EDEO A  003759032T:3]073ACNR  B00004147006 B00004148004 12" );
+            input >> *qm2;
+        }
 
         calc.new_trade( tm1 );
         calc.new_quote( qm );
-        BOOST_CHECK_EQUAL( callback_checked, false );
+        calc.new_quote( qm2 );
         calc.new_trade( tm2 );
+        BOOST_CHECK_EQUAL( callback_checked, false );
+        calc.new_trade( tm3 );
         BOOST_CHECK_EQUAL( callback_checked, true );
-
-
 
     }
  }
