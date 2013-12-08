3e3b55dacfe7fed03b86dbb48872e6fa6a51cd4e
Tramort <jake@gogo.by>
2013-12-08 18:18:02 +0300

    solutions/max_kaskevich/7/sources/minute_market_data_receiver/main.cpp: added code for work with minute_market_data.

--- solutions/max_kaskevich/7/sources/minute_market_data_receiver/main.cpp
+++ solutions/max_kaskevich/7/sources/minute_market_data_receiver/main.cpp
@@ -1,5 +1,5 @@
 #include "market_data_receiver.h"
-#include "async_writer.h"
+#include "minute_market_data.h"
 #include "config_reader.h"
 #include <fstream>
 #include <csignal>
@@ -13,11 +13,21 @@ void signal_handler( int )
     cond.notify_all();
 }
 
+using namespace minute_market;
 using namespace multicast_communication;
 
 int main()
 {
 
+    minute_market_data processor( BINARY_DIR );
+    config_reader config;
+    if ( !config.read( std::ifstream( BINARY_DIR "/config.ini" ) ) )
+    {
+        std::cout << "Can not find config.ini" << std::endl;
+        return -1;
+    }
+    config.processor = &processor;
+    receiver_ptr receiver = config.generate_receiver();  
     std::signal( SIGINT, signal_handler );
     boost::mutex::scoped_lock lock( mtx );
     cond.wait( lock );
