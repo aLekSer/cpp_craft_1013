1f63d8f9df03850dbc69a23fc0cfe2be75b51690
aLekSer <gdocse@gmail.com>
2013-12-14 14:48:51 +0200

    small changes, great increase in performance
    Signed-off-by: aLekSer <gdocse@gmail.com>

--- solutions/aleks_apalikov/Craft_7/sources/data_collector/main.cpp
+++ solutions/aleks_apalikov/Craft_7/sources/data_collector/main.cpp
@@ -3,14 +3,19 @@
 #include <string>
 #include <csignal>
 #include "Stock_receiver.h"
+#include "minute_market_data.h"
+#include "boost/thread.hpp"
 
 #include "main.h" // for tests only
 
 using namespace std;
-bool stop = false;
+//bool stop = false;
+boost::condition_variable stop;
+boost::mutex to_stop;
+
 void int_handler(int)
 {
-	stop = true;
+	stop.notify_one();
 }
 
 int main ( int argc, char* argv[] )
@@ -22,11 +27,13 @@ int main ( int argc, char* argv[] )
 
 	minute_market_data md;
 	cout << "Data_collector is running" << endl;
-	while (!stop)
+/*	while (!stop)
 	{
 		boost::this_thread::sleep_for( boost::chrono::milliseconds( 50 ) );
-	}
+	}*/
 
+	boost::mutex::scoped_lock lock_stop( to_stop );
+	stop.wait(lock_stop);
 	md.stop();
 	cout << "Program was interrupted, market data saved in market_data.dat output file, "
 		"check stats in sources/data/stats dir "<< endl;
