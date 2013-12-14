be31a0cfc8ab6899a843f95a443ae680f54d2d03
aLekSer <gdocse@gmail.com>
2013-12-14 15:01:59 +0200

    corrections before pull-request
    Signed-off-by: aLekSer <gdocse@gmail.com>

--- solutions/aleks_apalikov/Craft_7/sources/data_collector/main.cpp
+++ solutions/aleks_apalikov/Craft_7/sources/data_collector/main.cpp
@@ -6,8 +6,6 @@
 #include "minute_market_data.h"
 #include "boost/thread.hpp"
 
-#include "main.h" // for tests only
-
 using namespace std;
 //bool stop = false;
 boost::condition_variable stop;
@@ -21,23 +19,14 @@ void int_handler(int)
 int main ( int argc, char* argv[] )
 {
 	signal(SIGINT, int_handler);
-#ifdef _data_collector_test_
-	boost::thread send(&start_sending);
-#endif
 
 	minute_market_data md;
 	cout << "Data_collector is running" << endl;
-/*	while (!stop)
-	{
-		boost::this_thread::sleep_for( boost::chrono::milliseconds( 50 ) );
-	}*/
 
 	boost::mutex::scoped_lock lock_stop( to_stop );
 	stop.wait(lock_stop);
 	md.stop();
 	cout << "Program was interrupted, market data saved in market_data.dat output file, "
 		"check stats in sources/data/stats dir "<< endl;
-
-
 	return 0;
 }
